c2e190c0f4c72527f4db38092727abb4a9af2641
Galkin-Palkin <zaharzebra@mail.ru>
2022-11-24 12:46:12 +0300

    replace structs from main.rs to modules

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,3 @@
-use std::collections::BTreeSet;
-use chrono::{DateTime, Utc};
-
 fn main() {
-    println!("Hello, world!");
-}
-
-struct User {
-    id: i32,
-    login: String,
-    password_salt: String
-}
-
-enum ChildInstitutionData {
-    School(String),
-    KinderGarten(String)
-}
-
-struct ChildInstitution { // ???
-    institution_type: ChildInstitutionData,
-    address: String
-}
-
-struct ClassInfo {
-    teacher_id: i32,
-    students: BTreeSet<i32>,
-    number: String
+    println!("Hello, my server!");
 }
