af404d2b058573baa29ca567827df37f32b4aab5
Galkin-Palkin <zaharzebra@mail.ru>
2022-12-11 13:18:52 +0300

    make input field preview

--- app/src/main/java/com/myteacher/ui/primitives/InputField.kt
+++ app/src/main/java/com/myteacher/ui/primitives/InputField.kt
@@ -2,6 +2,8 @@ package com.myteacher.ui.primitives
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
 import androidx.compose.material.OutlinedTextField
 import androidx.compose.material.Text
 import androidx.compose.runtime.*
@@ -9,6 +11,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.myteacher.ui.theme.Theme
 
@@ -26,8 +29,20 @@ fun InputField(
             Text(text = header, style = headerStyle)
         },
         modifier = modifier
-            .background(Theme.colors.inputFieldBackground)
             .clip(Theme.shapes.textField)
+            .background(Theme.colors.primary)
             .border(1.dp, Theme.colors.border)
+    )
+}
+
+@Composable
+@Preview(showBackground = true)
+fun InputFieldPreview() {
+    InputField(
+        value = "Абракадабра",
+        onValueChange = {},
+        modifier = Modifier
+            .height(64.dp)
+            .width(200.dp)
     )
 }
