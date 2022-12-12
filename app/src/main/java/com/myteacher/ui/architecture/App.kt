48ec7537d585030524c6c75c777913f1223438f0
Galkin-Palkin <zaharzebra@mail.ru>
2022-12-12 12:41:40 +0300

    make custom button

--- app/src/main/java/com/myteacher/ui/architecture/App.kt
+++ app/src/main/java/com/myteacher/ui/architecture/App.kt
@@ -1,24 +1,11 @@
 package com.myteacher.ui.architecture
 
-import androidx.compose.foundation.layout.size
-import androidx.compose.runtime.*
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.unit.dp
-import com.myteacher.R
-import com.myteacher.ui.primitives.CheckBoxWithText
+import androidx.compose.runtime.Composable
 import com.myteacher.ui.theme.Theme
 
 @Composable
 fun App() {
     Theme {
-        var checked by remember { mutableStateOf(false) }
 
-        CheckBoxWithText(
-            checked = checked,
-            onCheckedChange = {checked = it},
-            text = stringResource(R.string.user_agreement),
-            checkBoxModifier = Modifier.size(40.dp)
-        )
     }
 }
---
+++ app/src/main/java/com/myteacher/ui/primitives/CustomButton.kt
@@ -0,0 +1,60 @@
+package com.myteacher.ui.primitives
+
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Button
+import androidx.compose.material.ButtonDefaults
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.myteacher.ui.theme.Theme
+import com.myteacher.R
+
+//TODO add disabled color
+@Composable
+fun CustomButton(
+    onClick: () -> Unit,
+    content: @Composable RowScope.() -> Unit,
+    modifier: Modifier = Modifier,
+    enabled: Boolean = true,
+    contentPadding: PaddingValues = PaddingValues()
+) {
+    Button(
+        onClick = onClick,
+        modifier = modifier,
+        enabled = enabled,
+        contentPadding = contentPadding,
+        colors = ButtonDefaults.buttonColors(
+            backgroundColor = Theme.colors.primary,
+            contentColor = Theme.colors.onPrimary,
+        ),
+        content = content,
+        shape = Theme.shapes.button,
+        elevation = ButtonDefaults.elevation(
+            defaultElevation = Theme.elevation.default,
+            pressedElevation = Theme.elevation.default,
+            focusedElevation = Theme.elevation.default,
+        )
+    )
+}
+
+@Composable
+@Preview
+fun ButtonPreview() {
+    CustomButton(
+        onClick = {},
+        content = {
+            Text(
+                text = stringResource(R.string.next),
+                style = Theme.typography.button.copy(textAlign = TextAlign.Center)
+            )
+        },
+        modifier = Modifier
+            .height(72.dp)
+            .width(480.dp)
+    )
+}
