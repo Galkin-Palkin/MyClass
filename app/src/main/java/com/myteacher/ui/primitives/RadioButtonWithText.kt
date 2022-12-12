6264362e8f08211efb22330000dbb6b4a14f8df6
Galkin-Palkin <zaharzebra@mail.ru>
2022-12-12 12:00:04 +0300

    make radio button with label

---
+++ app/src/main/java/com/myteacher/ui/primitives/RadioButtonWithText.kt
@@ -0,0 +1,28 @@
+package com.myteacher.ui.primitives
+
+import androidx.compose.foundation.layout.Row
+import androidx.compose.material.RadioButton
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun RadioButtonWithLabel(
+    selected: Boolean,
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier,
+    enabled: Boolean = true,
+    label: (@Composable () -> Unit)? = null
+) {
+    Row {
+        RadioButton(
+            selected = selected,
+            onClick = onClick,
+            enabled = enabled,
+            modifier = modifier
+        )
+
+        if (label != null) {
+            label()
+        }
+    }
+}
